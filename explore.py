541d2f978727f267a35639c125648b94a27b75d4
Josh Nicholas <hi@thambi.li>
2022-02-28 10:47:21 +1100

    Add files via upload

---
+++ explore.py
@@ -0,0 +1,38 @@
+#%%
+
+import pandas as pd 
+# from yachtcharter import yachtCharter
+# chart_key = f"oz-datablogs-something_random{igloo}"
+
+fillo = 
+
+#%%
+
+df = pd.read_csv(fillo)
+
+p = df
+
+print(p)
+print(p.columns.tolist())
+
+# #%%
+
+# template = [
+# 	{
+# 	"title": "Yacht Charter test chart",
+# 	"subtitle": "Things",
+# 	"footnote": "Footnote",
+# 	"source": "The universe",
+# 	"margin-left": "20",
+# 	"margin-top": "30",
+# 	"margin-bottom": "20",
+# 	"margin-right": "10"
+# 	}
+# ]
+
+# yachtCharter(template=template, 
+# 			data=final,
+# 			chartId=[{"type":"horizontalbar"}],
+# 			chartName=f"{chart_key}")
+
+# #%%
